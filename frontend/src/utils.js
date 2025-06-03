86c9e16e56f7dc56c68437f00b68bd73f250e0d2
RUPESH KUMAR <153922558+Rupeshkumar780@users.noreply.github.com>
2025-06-03 17:18:29 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,4 +1,5 @@
-export const BACKEND_URL =
-  import.meta.env.MODE === "development"
-    ?  "https://spectral-insights.onrender.com"
-    : "http://localhost:4001";
+export const BACKEND_URL = "https://spectral-insights.onrender.com";
+
+  // import.meta.env.MODE === "development"
+  //   ? 
+  //   : "http://localhost:4001";
