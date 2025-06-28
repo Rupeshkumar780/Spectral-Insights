6578d28b54af14bc638d1177922d50633be0380d
RUPESH KUMAR <rupesh780kumar@gmail.com>
2025-06-28 17:35:12 +0530

    ok

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,8 +3,10 @@ import Trending from "../home/Trending"
 import Devotional from "../home/Devotional"
 import Creator from "../home/Creator"
 import Technology from "../home/Technology"
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const Home = () => {
+  console.log("Backend" , BACKEND_URL);
   return(
     <>
     <div>
